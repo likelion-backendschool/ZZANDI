b2473b89716df0f80965d8dce1e5c2434abecc57
jhdl0157 <jhdl0157@naver.com>
2022-08-08 22:26:37 +0900

    Refactor : UserDto내부 클래스생성
    UserDto -> 내부 RegisterRequest, RegisterResponse 리팩토링

--- ZZANDI/src/main/java/com/ll/zzandi/dto/UserRequestDto.java
+++ ZZANDI/src/main/java/com/ll/zzandi/dto/UserRequestDto.java
@@ -1,10 +1,8 @@
 package com.ll.zzandi.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.ll.zzandi.domain.User;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,27 +10,46 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-@Getter @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class UserRequestDto {
-    @NotBlank
-    @Length(min = 3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
-    private String userId;
-    @NotBlank
-    @Length(min = 8, max = 50)
-    private String userPassword;
-    @Email
-    @NotBlank
-    private String userEmail;
+public class UserDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class RegisterRequest{
+        @NotBlank
+        @Length(min = 3, max = 20)
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+        private String userId;
+        @NotBlank
+        @Length(min = 8, max = 50)
+        private String userPassword;
+        @Email
+        @NotBlank
+        private String userEmail;
 
-    @NotBlank
-    @Length(min = 3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
-    private String userNickname;
+        @NotBlank
+        @Length(min = 3, max = 20)
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+        private String userNickname;
 
-    public void encodePassword(PasswordEncoder passwordEncoder){
-        this.userPassword=passwordEncoder.encode(this.userPassword);
+        public void encodePassword(PasswordEncoder passwordEncoder) {
+            this.userPassword = passwordEncoder.encode(this.userPassword);
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class RegisterResponse {
+        private String userId;
+        private String userEmail;
+        private String userNickname;
+        private boolean userEmailVerified;
+
+        @Builder
+        public RegisterResponse(User user){
+            this.userId=user.getUserId();
+            this.userEmail=user.getUserEmail();
+            this.userNickname=user.getUserNickname();
+            this.userEmailVerified=user.isUserEmailVerified();
+        }
     }
 }

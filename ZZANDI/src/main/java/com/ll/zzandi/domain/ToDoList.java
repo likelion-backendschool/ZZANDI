96bfcad278bf7e50b33fd3d36a13bb09a5443cf2
Young998904 <like0998904@gmail.com>
2022-08-10 00:33:34 +0900

    feat : todolist 에 어노테이션 추가

--- ZZANDI/src/main/java/com/ll/zzandi/domain/ToDoList.java
+++ ZZANDI/src/main/java/com/ll/zzandi/domain/ToDoList.java
@@ -2,6 +2,7 @@ package com.ll.zzandi.domain;
 
 import com.ll.zzandi.enumtype.Type;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,10 +10,13 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@Table(name = "TO_DO_LIST")
+@NoArgsConstructor
 public class ToDoList {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TODO_ID")
     private long id;
 
     @Column (name = "TODO_TITLE")
@@ -25,5 +29,6 @@ public class ToDoList {
     private String writer;
 
     @Column (name = "TODO_TYPE")
+    @Enumerated(EnumType.STRING)
     private Type type;
 }

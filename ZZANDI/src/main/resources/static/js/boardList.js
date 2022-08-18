5cd6c2f204d08e0be703dd7f930325eabedd3bd7
ddmkim94 <ddmkim94@gmail.com>
2022-08-18 21:34:10 +0900

    :sparkles: feat : 게시판에 비동기 페이징 적용

--- ZZANDI/src/main/resources/static/js/boardList.js
+++ ZZANDI/src/main/resources/static/js/boardList.js
@@ -1,25 +1,36 @@
 'use strict'
-
-const body = document.querySelector(".list");
-const pageList = document.querySelectorAll(".page-curr");
+/*
+    -> 페이징 구현에 필요한 데이터
+    현재 페이지 번호(nowPage) : Pageable.pageNumber (0부터 시작)
+    페이지 시작 번호(startPage) : ((현재 페이지  - 1) / pageSize) * 10 + 1
+    페이지 끝 번호(endPage) : (시작 번호 + pageSize) - 1;
+    다음 페이지 번호(nextPage) : 현재 페이지 + 1;
+    이전 페이지 번호(prevPage) : 현재 페이지 - 1;
+    이전 페이지 존재 여부(hasPrev) : (pageable.first == true) ? "이전 페이지 존재 X" : "이전 페이지 존재"
+    다음 페이지 존재 여부(hasNext) : (pageable.last == true) ? "다음 페이지 존재 X" : "다음 페이지 존재"
+    전체 게시물 데이터 개수(totalElements)
+    전체 페이지 개수(totalPages)
+*/
+const list = document.querySelector(".list");
+const pagination = document.querySelector('.pagination');
 
 window.onload = () => {
-    createListAjax(0);
+    findByPage(0);
 }
 
-function createListAjax(page) {
-    // for(let i = 0; i < pageList.length; i++) {
-    //     pageList[i].classList.remove("active");
-    // }
-    //
-    // const curr = pageList[page]; // 현재 페이지
-    // curr.classList.add("active");
-
+// 게시물 검색
+function findByPage(page) {
     fetch("/board/list-json?page=" + page)
         .then(response => response.json())
         .then(data => {
             const json = JSON.stringify(data);
             const items = JSON.parse(json);
+            // 게시물이 하나도 없는 경우
+            if(items.content.length === 0) {
+                list.innerHTML = '<td colspan="6">등록된 게시글이 없습니다.</td>';
+                createPageList();
+                return false;
+            }
             displayItems(items);
         });
 }
@@ -28,12 +39,12 @@ function createListAjax(page) {
  * 만들어진 list 목록을 <tbody>에 그려주는 함수
  */
 function displayItems(items) {
-    let list = document.querySelector(".list");
-    list.innerHTML = items.content.map(item => createHTMLString(item)).join('');
+    list.innerHTML = items.content.map(item => createBoardList(item)).join('');
+    pagination.innerHTML = createPageList(items);
 }
 
-// json 형태의 게시물 정보를 이용해서 리스트 row를 그려주는 함수
-function createHTMLString(item) {
+// 게시물 리스트 생성
+function createBoardList(item) {
     return `<tr>
                 <td style="color: mediumpurple;">${item.category}</td>
                 <td><a href="detail/${item.boardId}">${item.title}</a></td>
@@ -44,15 +55,54 @@ function createHTMLString(item) {
             </tr>`;
 }
 
+// 페이지 번호 리스트 생성
+function createPageList(items) {
+    let nowPage = items.pageable.pageNumber; // 시작 번호 0
+    let pageSize = items.pageable.pageSize;
+    let totalPage = items.totalPages; // 전체 페이지 개수
 
-let prev = document.querySelector(".prev-btn");
-let next = document.querySelector(".next-btn");
+    let startPage = Math.floor((nowPage) / pageSize) * pageSize; // 페이지 시작 번호
+    let endPage = startPage + pageSize; // 페이지 끝 번호
+    if (endPage > totalPage) endPage = totalPage;
+    let hasPrev = items.first; // 이전 페이지 존재 여부
+    let hasNext = items.last; // 다음 페이지 존재 여부
 
-// 이전, 다음 버튼 비활성화 함수
-function disabled(isCheck, type) {
-    if (type === 'prev' && isCheck === 'false') {
-        prev.removeAttribute('href');
-    } else if (type === 'next' && isCheck === 'false') {
-        next.removeAttribute('href');
+    let pageHTML = '';
+    // 처음 페이지, 이전 페이지
+    // 첫 페이지(0)인 경우 이전 버튼 disabled 속성 추가
+    const prevDisabled = hasPrev ? "disabled='disabled'" : '';
+    pageHTML +=
+        `<li>
+            <button class="page-btn" onClick="findByPage(0);" ${prevDisabled} aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </button>
+        </li>
+        <li>
+            <button class="page-btn" onClick="findByPage(${nowPage - 1});" ${prevDisabled} aria-label="Previous">
+                <span aria-hidden="true">&lsaquo;</span>
+            </button>
+        </li>`;
+
+    // 페이지 번호
+    for (let i = startPage; i < endPage; i++) {
+        const active = (i === nowPage) ? 'class="active"' : '';
+        pageHTML += `<li ${active}><button class="page-btn page-num" onclick="findByPage(${i})">${i + 1}</button></li>`;
     }
+
+    // 다음 페이지, 마지막 페이지
+    // 마지막 페이지인 경우 다음 버튼 disabled 속성 추가
+    const nextDisabled = hasNext ? "disabled='disabled'" : '';
+    pageHTML +=
+        `<li>
+            <button class="page-btn" onClick="findByPage(${nowPage + 1});" ${nextDisabled} aria-label="Next">
+                <span aria-hidden="true">&rsaquo;</span>
+            </button>
+        </li>
+        <li>
+            <button class="page-btn" onClick="findByPage(${totalPage - 1});" ${nextDisabled}  aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </button>
+        </li>`;
+
+    return pageHTML;
 }

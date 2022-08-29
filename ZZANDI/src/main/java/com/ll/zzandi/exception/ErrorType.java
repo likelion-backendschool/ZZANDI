fa9287cbf81ea9034711fa45043b4c6e458b3e68
jhdl0157 <jhdl0157@naver.com>
2022-08-29 16:44:00 +0900

    :recycle:  GlobalControllerAdvice 리팩토링

--- ZZANDI/src/main/java/com/ll/zzandi/exception/ErrorType.java
+++ ZZANDI/src/main/java/com/ll/zzandi/exception/ErrorType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ErrorType {
     DUPLICATED_USER_NAME("404", "User name is duplicated"),
     NOT_FOUND("404", "페이지를 찾을수 없습니다."),
-    INTERNAL_SERVER_ERROR("500", "서버에 문제가 있습니다."),
+    INTERNAL_SERVER_ERROR("500", "내부 서버에 문제가 있습니다."),
     ;
 
 

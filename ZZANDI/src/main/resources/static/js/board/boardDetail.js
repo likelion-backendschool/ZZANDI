5359ab8fe4fa94aae54a8d469354f53116e38d68
ddmkim94 <ddmkim94@gmail.com>
2022-09-08 18:16:09 +0900

    :art: feat : 게시글 상세 페이지에 토스트 UI 뷰어 도입

--- ZZANDI/src/main/resources/static/js/board/boardDetail.js
+++ ZZANDI/src/main/resources/static/js/board/boardDetail.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// 토스트 UI 뷰어
+const Editor = toastui.Editor;
+const viewer = new Editor.factory({
+    el: document.querySelector('#viewer'),
+    initialValue: document.querySelector(".board-content").value,
+    viewer: true,
+    plugins: [Editor.plugin.codeSyntaxHighlight]
+});
+
 function deleteBoard() {
     if (confirm("정말로 삭제하시겠습니까?") === false) {
         return false;

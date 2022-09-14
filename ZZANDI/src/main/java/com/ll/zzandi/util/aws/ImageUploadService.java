50cf1eab7c492f9161dbd35bb5de62453a03faa9
jhdl0157 <jhdl0157@naver.com>
2022-09-14 17:10:04 +0900

    :recycle: refactor : TransferManager is deprecated 문제 해결

--- ZZANDI/src/main/java/com/ll/zzandi/util/aws/ImageUploadService.java
+++ ZZANDI/src/main/java/com/ll/zzandi/util/aws/ImageUploadService.java
@@ -4,6 +4,7 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import com.amazonaws.services.s3.transfer.Upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,7 +25,7 @@ public class ImageUploadService {
     public String upload(String saveFileName, MultipartFile multipartFile) throws IOException {
         File file = new File(System.getProperty("user.dir") + saveFileName);
         multipartFile.transferTo(file);
-        final TransferManager transferManager = new TransferManager(this.amazonS3Client);
+        final TransferManager transferManager = TransferManagerBuilder.standard().withS3Client(amazonS3Client).build();
         System.out.println(saveFileName);
         final PutObjectRequest request = new PutObjectRequest(bucket, saveFileName, file);
         final Upload upload =  transferManager.upload(request);
@@ -38,5 +39,8 @@ public class ImageUploadService {
             file.delete();
         }
 
+    }
+
+    public void deletefile(String fileUrl) {
     }
 }

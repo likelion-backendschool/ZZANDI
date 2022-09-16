113c589b1ae9df85d2d650d5a6918152d9ecb0ed
jyajoo <jyajoo1020@gmail.com>
2022-09-16 18:04:37 +0900

    :recycle: del : 불필요한 select 제거

--- ZZANDI/src/main/java/com/ll/zzandi/service/TeamMateService.java
+++ ZZANDI/src/main/java/com/ll/zzandi/service/TeamMateService.java
@@ -13,7 +13,6 @@ import com.ll.zzandi.exception.TeamMateException;
 import com.ll.zzandi.exception.UserApplicationException;
 import com.ll.zzandi.repository.StudyRepository;
 import com.ll.zzandi.repository.TeamMateRepository;
-import com.ll.zzandi.repository.UserRepository;
 import com.ll.zzandi.util.mail.EmailMessage;
 import com.ll.zzandi.util.mail.EmailService;
 import java.util.Arrays;
@@ -27,23 +26,20 @@ import org.springframework.stereotype.Service;
 public class TeamMateService {
 
   private final TeamMateRepository teamMateRepository;
-  private final UserRepository userRepository;
   private final StudyRepository studyRepository;
   private final EmailService emailService;
   private final StudyService studyService;
 
   public void createTeamMate(User user, Long studyId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()-> new UserApplicationException(
-        ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()-> new StudyException(ErrorType.NOT_FOUND));
-    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(currentUser, study).orElse(null);
+    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(user, study).orElse(null);
 
     // 팀장과 일치하는 경우, Accepted인 상태로 팀원에 팀장 추가
-    if (study.getUser().getId().equals(currentUser.getId()) && teamMate == null) {
+    if (study.getUser().getId().equals(user.getId()) && teamMate == null) {
       teamMateRepository.save(new TeamMate(user, study, TeamMateStatus.ACCEPTED));
     } else if (teamMate == null) {
       teamMateRepository.save(new TeamMate(user, study, TeamMateStatus.WAITING));
-      sendWaitingEmail(currentUser, study);
+      sendWaitingEmail(user, study);
     } else {
       throw new TeamMateException(ErrorType.DUPLICATED_TEAMMATE);
     }
@@ -54,12 +50,11 @@ public class TeamMateService {
   }
 
   public void updateTeamMate(User user, Long studyId, Long teamMateId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()->new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()->new StudyException(ErrorType.NOT_FOUND));
     TeamMate teamMate = teamMateRepository.findById(teamMateId).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
 
     // 팀장만 수락이 가능
-    if(study.getUser() == currentUser) {
+    if(study.getUser() == user) {
       teamMate.setTeamMateStatus(TeamMateStatus.ACCEPTED);
       teamMateRepository.save(teamMate);
       sendAcceptedEmail(study, teamMate);
@@ -70,28 +65,26 @@ public class TeamMateService {
   }
 
   public boolean deleteTeamMate(User user, Long studyId, Long teamMateId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()->new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()->new StudyException(ErrorType.NOT_FOUND));
     TeamMate teamMate = teamMateRepository.findById(teamMateId).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
 
     boolean isLeader = false;
 
-    if(study.getUser() == currentUser) {
+    if(study.getUser() == user) {
       teamMateRepository.delete(teamMate);
       isLeader = true;
-    } else if (currentUser == teamMate.getUser()) {
+    } else if (user == teamMate.getUser()) {
       teamMateRepository.delete(teamMate);
     }
     return isLeader;
   }
 
   public void quitTeamMate(User user, Long studyId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()-> new StudyException(ErrorType.NOT_FOUND));
-    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(currentUser, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
+    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(user, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
 
     if((study.getStudyStatus() == StudyStatus.RECRUIT
-        || study.getStudyStatus() == StudyStatus.RECRUIT_COMPLETE) && study.getUser() != currentUser) {
+        || study.getStudyStatus() == StudyStatus.RECRUIT_COMPLETE) && study.getUser() != user) {
       teamMateRepository.delete(teamMate);
       study.setAcceptedStudyMember(study.getAcceptedStudyMember()-1);
       studyService.updateRecruitStudyStatus(study);
@@ -102,42 +95,39 @@ public class TeamMateService {
   }
 
   public void delegateTeamMate(User user, Long studyId, Long teamMateId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()-> new StudyException(ErrorType.NOT_FOUND));
-    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(currentUser, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
+    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(user, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
     TeamMate delegateTeamMate = teamMateRepository.findById(teamMateId).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
     User delegateUser = delegateTeamMate.getUser();
 
-    if (study.getUser() == currentUser && study.getUser() != delegateUser) {
+    if (study.getUser() == user && study.getUser() != delegateUser) {
       teamMate.setTeamMateDelegate(TeamMateDelegate.DELEGATE);
       delegateTeamMate.setTeamMateDelegate(TeamMateDelegate.WAITING);
-      sendDelegateEmail(study, currentUser, delegateUser);
+      sendDelegateEmail(study, user, delegateUser);
       teamMateRepository.saveAll(Arrays.asList(teamMate, delegateTeamMate));
     }
   }
 
   public void delegateTeamMateAccept(User user, Long studyId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()-> new StudyException(ErrorType.NOT_FOUND));
     User prev = study.getUser();
     TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(prev, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
 
     teamMateRepository.delete(teamMate);
     study.setAcceptedStudyMember(study.getAcceptedStudyMember()-1);
-    study.setUser(currentUser);
+    study.setUser(user);
     List<TeamMate> teamMateList = teamMateRepository.findByStudy(study);
     for (TeamMate teamMate1 : teamMateList) {
       teamMate1.setTeamMateDelegate(TeamMateDelegate.NONE);
       teamMateRepository.save(teamMate1);
     }
     studyService.updateRecruitStudyStatus(study);
-    sendDelegateAcceptEmail(prev, study, currentUser);
+    sendDelegateAcceptEmail(prev, study, user);
   }
 
   public void delegateRefuse(User user, Long studyId) {
-    User currentUser = userRepository.findByUserId(user.getUserId()).orElseThrow(()-> new UserApplicationException(ErrorType.NOT_FOUND));
     Study study = studyRepository.findById(studyId).orElseThrow(()-> new StudyException(ErrorType.NOT_FOUND));
-    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(currentUser, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
+    TeamMate teamMate = teamMateRepository.findByUserAndAndStudy(user, study).orElseThrow(()-> new TeamMateException(ErrorType.NOT_FOUND));
     teamMate.setTeamMateDelegate(TeamMateDelegate.NONE);
     teamMateRepository.save(teamMate);
   }
